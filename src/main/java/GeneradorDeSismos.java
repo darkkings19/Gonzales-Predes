c34e3e68edcde1d51eb462dd3b00de621d19b9c9
MCCGonzalez <m.gonzalez60@ufromail.cl>
2022-04-13 10:40:34 -0400

    segundo commit

--- src/main/java/GeneradorDeSismos.java
+++ src/main/java/GeneradorDeSismos.java
@@ -1,25 +1,50 @@
 import java.io.*;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class GeneradorDeSismos {
 
     public static void main(String[] args) {
 
-        Entrada();
+        int N;
+        double[][] matriz;
 
+        N = Entrada();
+        matriz = Matriz(N);
 
     }
-        public static int Entrada() {
-            Scanner teclado = new Scanner(System.in);
-            int N = teclado.nextInt();
-            if(N > 2  || N < 32){
 
+    public static int Entrada() {
+        Scanner teclado = new Scanner(System.in);
+        int N = teclado.nextInt();
+        if (N > 2 || N < 32) {
+
+        }
+        return N;
+    }
+
+    public static double[][] Matriz(int N) {
+
+        double[][] matriz = new double[N][24];
+        double max = 9.5;
+        double min = 1.0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 24; j++) {
+                matriz[i][j] = (double) (Math.random() * (max - min) + min);
+                System.out.println(matriz[i][j]);
             }
-            return N;
         }
+        return matriz;
+        
+    }
+}
 
-        }
+
+
+
+
 
 
 
