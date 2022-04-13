75ceb15069603e9362296fe8e9dc947dd98b936d
darkkings19 <p.paredes06@ufromail.cl>
2022-04-13 10:22:26 -0400

    commint incial

--- mayor.java
+++ mayor.java
@@ -1,12 +1,15 @@
 public class mayor {
     public static void main(String[] args) {
-        int[][] matriz = {{29, 23, 13}, {41, 57, 69}, {17, 28, 39}};
+        int[][] matriz = {{45, 66, 75}, {41, 99, 69}, {17, 28, 39}};
         int numeroMayor = matriz[0][0];
+
+        String mayorPosicion = "0,0";
         for (int x = 0; x < matriz.length; x++) {
             for (int y = 0; y < matriz[x].length; y++) {
                 if (matriz[x][y] > numeroMayor) {
                     numeroMayor = matriz[x][y];
-                    System.out.println("numero mayor"+ numeroMayor);
+                    mayorPosicion = x + "," + y;
+                    System.out.println("la posision: "+"("+ mayorPosicion +")"+ " y la intensidad fue " +numeroMayor);
                 }
             }
         }
