0704d6accf324b0a5454b5fb888397535cb5d988
MCCGonzalez <m.gonzalez60@ufromail.cl>
2022-04-13 09:50:22 -0400

    primer commit

---
+++ src/main/java/GeneradorDeSismos.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class GeneradorDeSismos {
+
+    public static void main(String[] args) {
+
+        Entrada();
+
+
+    }
+        public static int Entrada() {
+            Scanner teclado = new Scanner(System.in);
+            int N = teclado.nextInt();
+            if(N > 2  || N < 32){
+
+            }
+            return N;
+        }
+
+        }
+
+
+
